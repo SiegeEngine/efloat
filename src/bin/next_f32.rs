6787769321efb63e1891af6e8568c7280af667d7
Mike Dilger <mike@optcomp.nz>
2018-05-11 10:23:40 +1200

    next_f32 binary: print bits as well

--- src/bin/next_f32.rs
+++ src/bin/next_f32.rs
@@ -5,9 +5,17 @@ use std::env;
 
 fn main() {
     let arg1 = env::args().skip(1).next().unwrap();
-    let f: f32 = arg1.parse::<f32>().unwrap();
 
-    println!("f32: {}", f);
-    println!("Next f32 up: {}", efloat::next_f32_up(f));
-    println!("Next f32 down: {}", efloat::next_f32_down(f));
+    let f: f32 = arg1.parse::<f32>().unwrap();
+    let i: i32 = unsafe { ::std::mem::transmute(f) };
+
+    let up: f32 = efloat::next_f32_up(f);
+    let upi: i32 = unsafe { ::std::mem::transmute(up) };
+
+    let down: f32 = efloat::next_f32_down(f);
+    let downi: i32 = unsafe { ::std::mem::transmute(down) };
+
+    println!("f32: {} = 0x{:x}", f, i);
+    println!("Next f32 up: {} = 0x{:x}", up, upi);
+    println!("Next f32 down: {} = 0x{:x}", down, downi);
 }
